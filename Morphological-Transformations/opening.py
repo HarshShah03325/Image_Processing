21e13bcd1a2beb635aa44b2da78f594fe3183f4e
HarshShah03325 <64402716+HarshShah03325@users.noreply.github.com>
2021-08-26 15:26:36 +0530

    Add files via upload

---
+++ Morphological-Transformations/opening.py
@@ -0,0 +1,50 @@
+from PIL import Image
+import numpy as np
+
+def rgb2gray(rgb):
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+    return gray
+
+def gray2binary(gray):
+    return (127 < gray) & (gray <= 255)
+
+def dilation(image, kernel):
+    output = np.zeros_like(image)
+    image_padded = np.zeros((image.shape[0]+kernel.shape[0]-1,image.shape[1] + kernel.shape[1]-1))
+    image_padded[kernel.shape[0]-2:-1:,kernel.shape[1]-2:-1:] = image
+    for x in range(image.shape[1]):
+        for y in range(image.shape[0]):
+            summation=(kernel * image_padded[y: y+kernel.shape[0], x: x+kernel.shape[1]]).sum()
+            if summation>0:
+                output[y,x]=1
+            else :
+                output[y,x]=0
+    return output
+
+def erosion(image, kernel):
+    output = np.zeros_like(image)
+    image_padded = np.zeros((image.shape[0]+kernel.shape[0]-1,image.shape[1] + kernel.shape[1]-1))
+    image_padded[kernel.shape[0]-2:-1:,kernel.shape[1]-2:-1:] = image
+    for x in range(image.shape[1]):
+        for y in range(image.shape[0]):
+            summation=(kernel * image_padded[y: y+kernel.shape[0], x: x+kernel.shape[1]]).sum()
+            if summation==13:
+                output[y,x]=1
+            else :
+                output[y,x]=0
+    return output
+            
+
+structuring_element=np.array([[0, 0, 1, 0, 0],
+                              [0, 1, 1, 1, 0],
+                              [1, 1, 1, 1, 1],
+                              [0, 1, 1, 1, 0],
+                              [0, 0, 1, 0, 0]])
+
+
+file_name="morphological.png"
+im = gray2binary(rgb2gray(np.array(Image.open(file_name))))
+im=erosion(dilation(im,structuring_element),structuring_element)
+pil_img=Image.fromarray(im).convert('RGB')
+pil_img.save('opening_out.png')
---
+++ Morphological-Transformations/sobeledge.py
@@ -0,0 +1,38 @@
+from PIL import Image, ImageDraw
+from math import sqrt
+
+# Load image:
+input_image = Image.open("dilation_output.png")
+input_pixels = input_image.load()
+
+# Calculate pixel intensity as the average of red, green and blue colors.
+intensity = [[sum(input_pixels[x, y]) / 3 for y in range(input_image.height)] for x in range(input_image.width)]
+
+# Sobel kernels
+kernelx = [[-1, 0, 1],
+           [-2, 0, 2],
+           [-1, 0, 1]]
+kernely = [[1, 2, 1],
+           [0, 0, 0],
+           [-1, -2, -1]]
+
+# Create output image
+output_image = Image.new("RGB", input_image.size)
+draw = ImageDraw.Draw(output_image)
+
+# Compute convolution between intensity and kernels
+for x in range(1, input_image.width - 1):
+    for y in range(1, input_image.height - 1):
+        magx, magy = 0, 0
+        for a in range(3):
+            for b in range(3):
+                xn = x + a - 1
+                yn = y + b - 1
+                magx += intensity[xn][yn] * kernelx[a][b]
+                magy += intensity[xn][yn] * kernely[a][b]
+
+        # Draw in black and white the magnitude
+        color = int(sqrt(magx**2 + magy**2))
+        draw.point((x, y), (color, color, color))
+    
+output_image.save("sobel-edge-output.png")
